e7bb61559db17f7ad20ab031bb716435e21e5d7b
iregue <igna.regueiro1992@gmail.com>
2019-03-31 16:57:04 +0200

    first commit

---
+++ sketch_mar25a/sketch_mar25a.ino
@@ -0,0 +1,65 @@
+//Codigo ARDUINO NANO - EMISOR
+#include <SPI.h>
+#include <nRF24L01.h> // Libreria para la radio
+#include <RF24.h>
+
+//Declaremos los pines CE y el CSN del modulo de Radio
+#define CE_PIN 9
+#define CSN_PIN 10
+ 
+//Variable con la dirección del canal por donde se va a transmitir
+byte direccion[5] ={'S','E','D','1','9'};
+int id_modulo = 1;
+//creamos el objeto radio (NRF24L01)
+RF24 radio(CE_PIN, CSN_PIN);
+
+//vector con los datos a enviar
+float datos[3];
+
+void setup()
+{
+  //inicializamos el NRF24L01 
+  radio.begin();
+  //inicializamos el puerto serie
+  Serial.begin(9600); 
+ 
+//Abrimos un canal de escritura
+ radio.openWritingPipe(direccion);
+ 
+}
+ 
+void loop()
+{ 
+ int adc_MQ = analogRead(A0); //Lectura de la salida analogica A0 donde se encuentra el sensor MQ-135
+ float voltaje = adc_MQ * (5.0 / 1023.0); //Conversion de la lectura en un valor de voltaje
+ 
+ //cargamos los datos en la variable datos[]
+ datos[0]=id_modulo;
+ datos[1]=adc_MQ;
+ datos[2]=millis();
+ //enviamos los datos
+ bool ok = radio.write(datos, sizeof(datos));
+  //reportamos por el puerto serial los datos enviados 
+  if(ok && adc_MQ >=10) //PARA TEST adc_MQ > 10. PARA DETECTAR HUMO adc_MQ >= 110
+  {
+     Serial.print("Datos enviados: "); 
+     Serial.print(datos[0]); 
+     Serial.print(" , "); 
+     Serial.print(datos[1]); 
+     Serial.print(" , "); 
+     Serial.println(datos[2]); 
+  }
+  else
+  {
+     Serial.println("no se ha podido enviar");
+  }
+ //Si se detecta humo se activa la alerta sonora
+ if(adc_MQ >=110){
+  tone(7,294,5000); //Se manda señal a pin D7 sonido con frecuendia de 294 Hz, suena durante 5000 ms
+ }
+ else{
+  noTone(7);
+ }
+  delay(1000);
+}
+ 
---
+++ sketch_mar28a/sketch_mar28a.ino
@@ -0,0 +1,71 @@
+//Codigo ARDUINO UNO - RECEPTOR
+#include <SPI.h>
+#include <nRF24L01.h> //Libreria para la radio
+#include <RF24.h>
+#include <LiquidCrystal.h>
+ 
+//Declaremos los pines CE y el CSN del modulo de radio
+#define CE_PIN 9
+#define CSN_PIN 10
+
+LiquidCrystal lcd(2,3,5,4,6,8);
+//Variable con la dirección del canal que se va a leer
+byte direccion[5] ={'S','E','D','1','9'}; 
+
+//creamos el objeto radio (NRF24L01)
+RF24 radio(CE_PIN, CSN_PIN);
+
+//vector para los datos recibidos
+float datos[3];
+
+void setup()
+{
+  lcd.begin(16,2);
+ //inicializamos el NRF24L01 
+  radio.begin();
+  //inicializamos el puerto serie
+  Serial.begin(9600); 
+  
+  //Abrimos el canal de Lectura
+  radio.openReadingPipe(1, direccion);
+  
+    //empezamos a escuchar por el canal
+  radio.startListening();
+ 
+}
+ 
+void loop() {
+ uint8_t numero_canal;
+ lcd.print("Hola Luis");
+ lcd.setCursor(0,1);
+ lcd.print("Ya hay pantalla");
+ if ( radio.available() )
+ {    
+     //Leemos los datos y los guardamos en la variable datos[]
+     radio.read(datos,sizeof(datos));
+     
+     //reportamos por el puerto serial los datos recibidos
+     Serial.print("ID MODULO= " );
+     Serial.print(datos[0]);
+     Serial.print(" , ");
+     Serial.print("SENSOR= " );
+     Serial.print(datos[1]);
+     Serial.print(" , ");
+     Serial.print("MILLIS= " );
+     Serial.print(datos[2]);
+     Serial.println(" ms.");
+     //Si se recibe señal de radio con niveles de humo se activa la alerta sonora
+     if((int)datos[1]>=110){
+      tone(7,294,5000); //Se manda señal a pin D7 sonido con frecuendia de 294 Hz, suena durante 5000 ms
+     }
+     else{
+      noTone(7);
+     }
+ }
+ else
+ {
+     Serial.println("No hay datos de radio disponibles");
+ }
+ delay(1000);
+ lcd.clear();
+}
